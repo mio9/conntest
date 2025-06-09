e77470ead39173b394056b62da34f42d6c8f2aa8
Mio Wong <mio.wong@swivelsoftware.com>
2025-06-09 16:12:15 +0800

    add better logging

--- server/main.go
+++ server/main.go
@@ -3,34 +3,45 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	fmt.Fprintf(conn, "Welcome! Type 'quit' to exit.\n")
+
+	log.Println("New connection from", conn.RemoteAddr())
 
 	reader := bufio.NewReader(conn)
+	lastMsg := "" // Store the last received message
 	for {
 		message, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading:", err)
 
+		if err != nil {
+			log.Println("Error reading:", err)
+			log.Printf("Last message is %s", lastMsg)
 			break
 		}
-
-		fmt.Printf("Received: %s", message)
+		lastMsg = message
+		// fmt.Printf("Received: %s", message)
 
 		_, err = conn.Write([]byte("Echo: " + message))
 		if err != nil {
-			fmt.Println("Error writing:", err)
+			log.Println("Error writing:", err)
+			log.Printf("Last message is %s", lastMsg)
 			break
 		}
 	}
 }
 
 func main() {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Current directory: %s\n", path)
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
